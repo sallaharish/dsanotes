5d8eecbfdcc378142121fda523efb26a61dcfade
Salla Harish <sallaharish040@gmail.com>
2025-10-03 23:03:38 +0530

    1st day

---
+++ 1_Programming101.js
@@ -0,0 +1,50 @@
+console.log("Hello world");
+console.log("Akshay sainin");
+
+// string a="12"
+// number a=112
+
+const a = 12;
+let b = 12
+var d = 12
+
+console.log(a + b);
+
+console.log(a * b);
+
+
+let first = "harish";
+let last = "salla";
+console.log(first + " " + last)
+
+
+// array
+
+let arr = [23, 234, "234", null]
+console.log(arr[0]);
+
+console.log(arr[0] + arr[1] + arr[10])
+
+
+// array can contain mixed of number and digits also it can be any data type
+
+// we can have multiple array inside anothe array
+
+
+let obj = {
+    "name": "harissh",
+    "age": "q23"
+}
+
+console.log(obj.name)
+
+
+// we have controll 0ver the key in the case of the objects
+
+// obj.name
+// obj.age
+
+// in this way we can see the multiple v alue over the data 
+
+
+// similarly we can do teh mathematical operations as well in the objects

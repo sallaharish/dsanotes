086cff0f65086b58bfa482cd138b71794469e9a4
Salla Harish <sallaharish040@gmail.com>
2025-10-16 12:44:01 +0530

    remove duplicates

---
+++ 2_Array(easy/medium)/removeDuplicates.js
@@ -0,0 +1,12 @@
+// leet code 66 problem
+
+ var removeDuplicates = function(nums) {
+      let x = 0;
+      for (let i = 0; i < nums.length; i++) {
+          if (nums[i] > nums[x]) {
+              x++;
+              nums[x] = nums[i];
+          }
+      }
+      return x + 1;
+  }; 
